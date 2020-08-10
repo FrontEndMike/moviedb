4c9f90039bfe8dd6769ece3ae9a5cb912f0796c3
Mike <michael.lenard.jackson@gmail.com>
2020-08-09 20:44:40 -0500

    Updates

---
+++ precache-manifest.452cecc6f02d562e93a8c1700c9b526b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6b2cedced9c1855010ae1962659a9d64",
+    "url": "/moviedb/index.html"
+  },
+  {
+    "revision": "8dc372a70fc8a7c9402a",
+    "url": "/moviedb/static/css/main.3cfecfff.chunk.css"
+  },
+  {
+    "revision": "b93797d0f2ad4228ff1b",
+    "url": "/moviedb/static/js/2.c8fb5464.chunk.js"
+  },
+  {
+    "revision": "4bf00ea42f5dad8ab33d5f32afec9baf",
+    "url": "/moviedb/static/js/2.c8fb5464.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8dc372a70fc8a7c9402a",
+    "url": "/moviedb/static/js/main.359f8ef5.chunk.js"
+  },
+  {
+    "revision": "dff7d7152d1d72c996c0",
+    "url": "/moviedb/static/js/runtime-main.c7335139.js"
+  }
+]);
