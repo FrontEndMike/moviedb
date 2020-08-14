f7e94a37205b025ab482b98c5156bfe1e032c25e
Mike <michael.lenard.jackson@gmail.com>
2020-08-14 18:10:40 -0500

    Movie list showing results of a specific query

--- src/moviesList.js
+++ src/moviesList.js
@@ -5,7 +5,23 @@ import Movie from './movies';
 class moviesList extends PureComponent {
     state = {
         movies: []
-}  
+}
+  
+async componentDidMount() {
+    try {
+    const SEARCH_QUERY = "The Godfather";
+    const API_KEY = "a62fd138fc3adf6aa51790c63f1f498e";
+    const API_URL = `https://api.themoviedb.org/3/search/movie?api_key=${API_KEY}&language=en-US&query=${SEARCH_QUERY}&page=1&include_adult=false`;
+    const res = await fetch(`${API_URL}`);
+    const movies = await res.json();
+    this.setState({
+        movies: movies.results
+    })
+    } catch(e){
+    console.log(e);
+    }
+}
+    
 	render() {
     return (
         <MovieGrid>
